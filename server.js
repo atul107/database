0c38cfc0472fe36c1e5aea3f559b52194364ff9f
Atul <atul.kumar@front.page>
2023-07-26 00:22:05 +0530

    Fix error

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const { google } = require('googleapis');
-const cors = require('cors'); 
-
-
+const cors = require('cors');
 const app = express();
-const port = 3000; 
+const port = 3000;
 
-const allowedOrigins = ['https://magnificent-kelpie-5e7738.netlify.app/'];
+const allowedOrigins = ['*'];
 
 // Use the cors middleware with specific options
 app.use(cors({
@@ -19,38 +17,41 @@ app.use(cors({
   }
 }));
 
-const apiKey = 'AIzaSyAl4fRk6B9cBZfpaR2auQUI-MouNoWld7Q';
-
-const sheets = google.sheets({
-  version: 'v4',
-  auth: apiKey,
-});
-
-app.post('/saveUserData', express.json(), (req, res) => {
+app.post('/saveUserData', express.json(), async (req, res) => {
   const { username, mobileNumber } = req.body;
 
   const spreadsheetId = '1KblXjAKssuutBElGZVuXjMIPKYq8ZWsoCT-48GDX8RA';
-
   const sheetName = 'Sheet1';
-
   const values = [[username, mobileNumber]];
 
-  sheets.spreadsheets.values.append({
-    spreadsheetId,
-    range: sheetName,
-    valueInputOption: 'RAW',
-    resource: {
-      values,
-    },
-  }, (err, response) => {
-    if (err) {
-      console.error('The API returned an error:', err);
-      return res.status(500).json({ error: 'Failed to save data to Google Sheet' });
-    }
+  try {
+    // Load credentials from 'credentials.json'
+    const auth = new google.auth.GoogleAuth({
+        keyFile: 'credentials.json',
+        scopes: 'https://www.googleapis.com/auth/spreadsheets'
+    })
+
+    const client = await auth.getClient();
+    const sheets = google.sheets({
+      version: 'v4',
+      auth: client,
+    });
+
+    const response = await sheets.spreadsheets.values.append({
+      spreadsheetId,
+      range: sheetName,
+      valueInputOption: 'RAW',
+      resource: {
+        values,
+      },
+    });
 
     console.log('Data saved successfully:', response.data);
     res.status(200).json({ message: 'Data saved successfully' });
-  });
+  } catch (err) {
+    console.error('The API returned an error:', err);
+    res.status(500).json({ error: 'Failed to save data to Google Sheet' });
+  }
 });
 
 // Start the server
